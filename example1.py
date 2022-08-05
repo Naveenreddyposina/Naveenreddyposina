0e7e8e4fb06b683c0e7a468d0a9bda2e4493278f
Naveenreddyposina <110577890+Naveenreddyposina@users.noreply.github.com>
2022-08-05 06:56:04 +0530

    Add files via upload
    added string file

---
+++ example1.py
@@ -0,0 +1,18 @@
+s='banglore is a nice city'
+print(type(s))
+b='anantapur'
+print(type(b))
+print('upper result',s.upper())
+print('line number 6 output ###',s.capitalize())
+print(s.title())
+#syntax to use string operations
+#print(<any_string_variable>.<string_operation_name>())
+print(dir(s))
+#syntax to check the datatype operations
+#print(dir(<any_data_type_veriable>))
+
+
+
+
+
+
